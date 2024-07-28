63f69105437b3417bd96140a791bbeb961a467eb
Naveen <notnaveen2@gmail.com>
2024-07-28 05:52:42 +0530

    Made padding changes to BigTerminal

--- src/components/BigTerminal.tsx
+++ src/components/BigTerminal.tsx
@@ -1,12 +1,12 @@
 export const BigTerminal = () => {
   return (
     <div className="terminal_root h-[600px] w-[800px] bg-[#17161f] grid grid-rows-[30px_1fr] rounded-lg   pl-2 ">
-      <div className="terminalHeader ">
+      <div className="terminalHeader pl-1 ">
         <span className="bg-[#ff5f56] inline-block rounded-full h-[12px] w-[12px] ml-[0px] mt-[8px] "></span>
         <span className="bg-[#ffbd2e]  inline-block rounded-full h-[12px] w-[12px] ml-[8px] mt-[8px] "></span>
         <span className="bg-[#27c93f]  inline-block rounded-full h-[12px] w-[12px] ml-[8px] mt-[8px] "></span>
       </div>
-      <div className="terminalText">
+      <div className="terminalText pl-1">
         <p>
           {" "}
           <span className="text-green-500 font-semibold">$</span>{" "}
