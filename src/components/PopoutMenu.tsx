f5e9095b081c40912c7e361bfd28c7f965214fbd
Naveen <notnaveen2@gmail.com>
2024-07-28 05:35:20 +0530

    Download Files Linked

--- src/components/PopoutMenu.tsx
+++ src/components/PopoutMenu.tsx
@@ -5,41 +5,32 @@ interface Props {
 }
 
 export const PopoutMenu = ({ status }: Props) => {
-  const handleLinuxDownload = () => {
-    const fileUrl = ".../public/DownloadFiles/Linux/init";
-    const a = document.createElement("a");
-    a.href = fileUrl;
-    a.download = ""; // This attribute makes the browser download the file instead of navigating to it
-
-    // Append the anchor to the document body
-    document.body.appendChild(a);
-    a.click(); // Programmatically click the anchor to trigger the download
-
-    // Remove the anchor from the document
-    document.body.removeChild(a);
-  };
-
   return (
     <div
       className={`${
         status ? "inline" : "hidden"
       } absolute flex gap-2 left-[245px] bottom-[300px] `}
     >
-      <div
-        onClick={handleLinuxDownload}
+      <a
+        rel="noreferrer noopener"
+        href="https://github.com/capGoblin/init/tree/main/public/DownloadFiles/Linux"
+        target="_blank"
         className={`border cursor-pointer ${buttonVariants({
           variant: "secondary",
         })}`}
       >
         For Linux
-      </div>
-      <div
+      </a>
+      <a
+        rel="noreferrer noopener"
+        href="https://github.com/capGoblin/init/tree/main/public/DownloadFiles/Windows"
+        target="_blank"
         className={`border cursor-pointer ${buttonVariants({
           variant: "secondary",
         })}`}
       >
         For Windows
-      </div>
+      </a>
     </div>
   );
 };
